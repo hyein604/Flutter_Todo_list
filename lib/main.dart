bd45ecaadc80b5829dc4819e00678f7f690df315
hyein604 <hyein66044.gmail.com>
2023-08-07 20:02:23 +0900

    수정 및 삭제 기능 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo_list/task.dart';
 import 'package:intl/intl.dart';
 import 'package:percent_indicator/linear_percent_indicator.dart';
 
@@ -32,6 +33,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final _textController = TextEditingController();
+  List<Task> tasks =[];
 
   String getToday() {
     DateTime now = DateTime.now();
@@ -64,7 +66,18 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                   ),
                   ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      //add버튼 눌렀을때 기능
+                      if(_textController.text ==''){ //칸이 비어있다면
+                        return;
+                        }else{ //칸이 내용이 있다면
+                          setState(() {
+                            var task = Task(_textController.text);
+                            tasks.add(task);
+                            _textController.clear();
+                          });
+                        }
+                    },
                     child: const Text("Add"),
                   )
                 ],
@@ -83,37 +96,42 @@ class _MyHomePageState extends State<MyHomePage> {
                 ],
               ),
             ),
-            Row(
-              children: [
-                Flexible(
-                  child: TextButton(
-                    style: TextButton.styleFrom(
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.zero),
+            for(var i=0; i<tasks.length; i++)
+               Row(
+                children: [
+                  Flexible(
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.zero),
+                        ),
                       ),
-                    ),
-                    onPressed: () {},
-                    child: const Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Row(
-                        children: [
-                          Icon(Icons.check_box_outline_blank_rounded),
-                          Text("todo1"),
-                        ],
+                      onPressed: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            const Icon(Icons.check_box_outline_blank_rounded),
+                            Text(tasks[i].work)
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                ),
-                TextButton(
-                  onPressed: () {},
-                  child: const Text("수정"),
-                ),
-                TextButton(
-                  onPressed: () {},
-                  child: const Text("삭제"),
-                ),
-              ],
-            ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text("수정"),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        tasks.remove(tasks[i]);
+                      });
+                    },
+                    child: const Text("삭제"),
+                  ),
+                ],
+              ),
           ],
         ),
       ),
